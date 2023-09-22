22ea8aaad76aef882087c70156a48d02a2791181
validcube <pun.butrach@gmail.com>
2023-09-22 20:55:54 +0700

    feat: theme switcher

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,76 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+import 'package:validcube_gh/pages/blog.dart';
 import 'package:validcube_gh/pages/contribution.dart';
+import 'package:validcube_gh/pages/credits.dart';
 import 'package:validcube_gh/pages/home.dart';
 import 'package:validcube_gh/pages/portfolio.dart';
 import 'package:validcube_gh/pages/project.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
-  runApp(const MyApp());
+  LicenseRegistry.addLicense(() async* {
+    final license = await rootBundle.loadString('google_fonts/OFL.txt');
+    yield LicenseEntryWithLineBreaks(['google_fonts'], license);
+  });
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ThemeProvider(),
+      child: const MyApp(),
+    ),
+  );
 }
+
+class ThemeProvider extends ChangeNotifier {
+  bool _isDark = false;
+  bool get isDark => _isDark;
+  ThemeData get currentTheme => _isDark ? darkTheme : lightTheme;
+
+  void toggleTheme() {
+    _isDark = !_isDark;
+    notifyListeners();
+  }
+}
+
+final lightTheme = ThemeData(
+  brightness: Brightness.light,
+  fontFamily: GoogleFonts.robotoFlex().fontFamily,
+  colorScheme: ColorScheme.fromSeed(
+    brightness: Brightness.light,
+    seedColor: const Color(0xCC984061),
+  ),
+  useMaterial3: true,
+);
+
+final darkTheme = ThemeData(
+  brightness: Brightness.dark,
+  fontFamily: GoogleFonts.robotoFlex().fontFamily,
+  colorScheme: ColorScheme.fromSeed(
+    brightness: Brightness.dark,
+    seedColor: const Color(0xCC984061),
+  ),
+  useMaterial3: true,
+);
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context);
     return MaterialApp(
+      debugShowCheckedModeBanner: true,
       title: 'validcube.github.io',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xCC984061)),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      theme: themeProvider.currentTheme,
+      home: const MyHomePage(title: 'validcube'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
@@ -48,11 +83,23 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context);
     return Scaffold(
       body: SafeArea(
         child: Row(
           children: <Widget>[
             NavigationRail(
+              selectedLabelTextStyle: TextStyle(
+                color: Theme.of(context).colorScheme.onSurface,
+                fontSize: 12.0,
+                fontWeight: FontWeight.w700,
+              ),
+              unselectedLabelTextStyle: TextStyle(
+                color: Theme.of(context).colorScheme.onSurfaceVariant,
+                fontSize: 12.0,
+                fontWeight: FontWeight.w500,
+              ),
+              trailing: _themeToggle(themeProvider, context),
               labelType: NavigationRailLabelType.all,
               selectedIndex: _currentIndex,
               groupAlignment: -1,
@@ -61,43 +108,110 @@ class _MyHomePageState extends State<MyHomePage> {
                   _currentIndex = index;
                 });
               },
-              destinations: const <NavigationRailDestination>[
-                NavigationRailDestination(
-                  icon: Icon(Icons.home_outlined),
-                  selectedIcon: Icon(Icons.home),
-                  label: Text('Home'),
-                ),
-                NavigationRailDestination(
-                  icon: Icon(Icons.bookmark_border),
-                  selectedIcon: Icon(Icons.bookmark),
-                  label: Text('Portfolio'),
-                ),
-                NavigationRailDestination(
-                  icon: Icon(Icons.book_outlined),
-                  selectedIcon: Icon(Icons.book),
-                  label: Text('Project'),
-                ),
-                NavigationRailDestination(
-                  icon: Icon(Icons.star_outline),
-                  selectedIcon: Icon(Icons.star),
-                  label: Text('Contribution'),
-                ),
-              ],
+              destinations: _navigationDestinations(),
             ),
-
             const VerticalDivider(thickness: 1, width: 1),
-            // This is the main content.
             Expanded(
-              child: <Widget>[
-                const Home(),
-                const Portfolio(),
-                const Project(),
-                const Contribution()
-              ][_currentIndex],
+              child: AnimatedSwitcher(
+                switchInCurve: Curves.easeInOutCubic,
+                switchOutCurve: Curves.easeInOutCubic,
+                duration: const Duration(milliseconds: 300),
+                transitionBuilder: (Widget child, Animation<double> animation) {
+                  return FadeTransition(
+                    opacity: animation,
+                    child: child,
+                  );
+                },
+                child: _page(_currentIndex),
+              ),
             ),
           ],
         ),
       ),
     );
+  }
+
+  List<NavigationRailDestination> _navigationDestinations() {
+    return const <NavigationRailDestination>[
+      NavigationRailDestination(
+        padding: EdgeInsets.only(top: 16.0),
+        icon: Icon(Icons.home_outlined),
+        selectedIcon: Icon(Icons.home),
+        label: Text('Home'),
+      ),
+      NavigationRailDestination(
+        icon: Icon(Icons.bookmark_border),
+        selectedIcon: Icon(Icons.bookmark),
+        label: Text('Portfolio'),
+      ),
+      NavigationRailDestination(
+        icon: Icon(Icons.book_outlined),
+        selectedIcon: Icon(Icons.book),
+        label: Text('Project'),
+      ),
+      NavigationRailDestination(
+        icon: Icon(Icons.emoji_events_outlined),
+        selectedIcon: Icon(Icons.emoji_events),
+        label: Text('Contribution'),
+      ),
+      NavigationRailDestination(
+        icon: Icon(Icons.rss_feed_outlined),
+        selectedIcon: Icon(Icons.rss_feed),
+        label: Text('Blog'),
+      ),
+      NavigationRailDestination(
+        icon: Icon(Icons.star_outline),
+        selectedIcon: Icon(Icons.star),
+        label: Text('Credits'),
+      ),
+    ];
+  }
+
+  Widget _themeToggle(ThemeProvider themeProvider, BuildContext context) {
+    return Expanded(
+      child: Align(
+        alignment: Alignment.bottomCenter,
+        child: Padding(
+          padding: const EdgeInsets.only(bottom: 16.0),
+          child: ElevatedButton(
+            onPressed: () {
+              themeProvider.toggleTheme();
+            },
+            style: ElevatedButton.styleFrom(
+              shape: CircleBorder(
+                side: BorderSide(
+                  color: Theme.of(context).colorScheme.outline,
+                ),
+              ),
+              padding: const EdgeInsets.all(20),
+              backgroundColor: Theme.of(context).colorScheme.surface,
+            ),
+            child: Icon(
+              themeProvider.isDark ? Icons.light_mode : Icons.dark_mode,
+              color: Theme.of(context).colorScheme.onSurface,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _page(int currentIndex) {
+    switch (currentIndex) {
+      case 0:
+        return const Home();
+      case 1:
+        return const Portfolio();
+      case 2:
+        return const Project();
+      case 3:
+        return const Contribution();
+      case 4:
+        return const Blog();
+      case 5:
+        return const Credits();
+      default:
+        return const Home(); // Handle invalid index gracefully.
+    }
   }
 }
